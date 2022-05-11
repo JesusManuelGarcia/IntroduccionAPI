8ebb161173e4148afac496cc0f20364c1188c8c1
JesúsManuelGarcia <jesus-manuel-gar2000@hotmail.com>
2022-05-11 11:47:11 -0600

    se configuro la ruta de usuarios para la base de datos

--- ProyectoExpress/routes/routes.js
+++ ProyectoExpress/routes/routes.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const pool = require ('../data/config');
 const router=app=>{
     app.get('/', (request, response)=>{
@@ -5,4 +6,11 @@ const router=app=>{
             message: 'Bienvenido a Node.js Express REST API!'
         });
     });
+app.get('/users',(request, response)=>{
+    pool.query('SELECT * FROM users', (error, result)=>{
+        if(error)throw error;
+
+        response.send(result);
+    });
+});
 }
