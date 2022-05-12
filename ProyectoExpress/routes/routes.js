7457f126b83a99a3c03ddc4ae1881e593b7ac82a
JesúsManuelGarcia <jesus-manuel-gar2000@hotmail.com>
2022-05-12 11:20:48 -0600

    se grega ruta para agregar id del usuario

--- ProyectoExpress/routes/routes.js
+++ ProyectoExpress/routes/routes.js
@@ -21,5 +21,14 @@ app.get('/users/:id',(request, response)=>{
         response.send(result);
     });
 });
+app.post('/users',(request, response)=>{
+        pool.query('INSERT INTO users SET ?',request.body, (error, result)=>{
+        if(error)throw error;
+
+        response.status(201).send(`User added with ID: ${result.insertId}`);
+    });
+});
+
+
 
 }
