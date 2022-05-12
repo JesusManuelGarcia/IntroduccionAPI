eb5a6c93b71e8f6d9d04d7b9283ebbc6ca74ca1e
JesúsManuelGarcia <jesus-manuel-gar2000@hotmail.com>
2022-05-12 11:57:45 -0600

    cambios

--- ProyectoExpress/data/config.js
+++ ProyectoExpress/data/config.js
@@ -2,7 +2,7 @@ const mysql = require ('mysql');
 const config={
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'qwerty',
     database: 'api',
 };
 const pool = mysql.createPool(config);
